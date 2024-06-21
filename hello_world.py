de6ba71d81c12fd69cd5aebd98747c1bdb2ddec5
MichelleWairimu <nimmunduati17@gmail.com>
2024-06-21 09:28:39 +0300

    First commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+from asyncflows import AsyncFlows
+
+
+async def main():
+    # Load the flow from the file
+    flow = AsyncFlows.from_file("hello_world.yaml")
+
+    # Run the flow
+    result = await flow.set_vars(name="Michelle").run()
+    # Print the result
+    print(result)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
